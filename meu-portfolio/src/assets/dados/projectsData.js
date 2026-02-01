1dcc2c6d22a5590370df40115a46dc5363c62df0
Camomila <camilacs1402@gmail.com>
2026-02-01 18:03:59 -0300

    mudança das imgs para pasta public por causa do deploy

--- meu-portfolio/src/assets/dados/projectsData.js
+++ meu-portfolio/src/assets/dados/projectsData.js
@@ -3,13 +3,13 @@ export const projectsData = [
         id: 1,
         title: "EuComo",
         description: "Jogo desenvolvido como projeto final para a disciplina de Computação Gráfica na UNESP. O foco foi a aplicação de conceitos para renderização 3D, controle de câmera e física de movimento utilizando C# e Unity. Aqui, exercitei bastante o design de interfaces e menus.",
-        image: "/src/assets/imgs/Eucomo/eucomo.png",
+        image: "/assets/Eucomo/eucomo.png",
         images: [
 
-        "/src/assets/imgs/Eucomo/eucomo.png",
-        "/src/assets/imgs/Eucomo/EuComoDemo.mp4",
-        "/src/assets/imgs/Eucomo/eucomo2.png",
-        "/src/assets/imgs/Eucomo/eucomo3.png"
+        "/assets/Eucomo/eucomo.png",
+        "/assets/Eucomo/EuComoDemo.mp4",
+        "/assets/Eucomo/eucomo2.png",
+        "/assets/Eucomo/eucomo3.png"
 
         ],
         githubLink: "https://github.com/milacs1402/EuComo",
@@ -19,10 +19,10 @@ export const projectsData = [
         id: 2,
         title: "Milafy",
         description: "Spotify lookalike desenvolvido durante a Imersão Front-End da Alura. Foquei na componentização de elementos, manipulação dinâmica do DOM com JavaScript e estilização responsiva utilizando conceitos avançados de CSS Grid e Flexbox.",
-        image: "/src/assets/imgs/milafy.png",
+        image: "/assets/milafy.png",
         images: [
 
-        "/src/assets/imgs/milafy.png",
+        "/assets/milafy.png",
 
         ],
         githubLink: "https://github.com/milacs1402/Milafy",
@@ -32,12 +32,12 @@ export const projectsData = [
         id: 3,
         title: "Overdle",
         description: "Um dos meus primeiros projetos explorando React. Embora tenha muitos pontos a melhorar (principalmente em responsividade), foi onde consolidei diversos conceitos, além de ter tido a oportunidade de recriar com meu amigo um dos nossos jogos favoritos.",
-        image: "/src/assets/imgs/overdle/overdle.png",
+        image: "/assets/overdle/overdle.png",
         images: [
 
-        "/src/assets/imgs/overdle/overdle.png",
-        "/src/assets/imgs/overdle/overdle2.png",
-        "/src/assets/imgs/overdle/overdle3.png"
+        "/assets/overdle/overdle.png",
+        "/assets/overdle/overdle2.png",
+        "/assets/overdle/overdle3.png"
 
         ],
         githubLink: "https://github.com/milacs1402/overdle",
@@ -47,10 +47,10 @@ export const projectsData = [
         id: 4,
         title: "Portifólio",
         description: "Uma vitrine da minha estética como desenvolvedora. Aqui, explorei a criação de uma identidade visual própria e a implementação de um layout responsivo, transformando o currículo tradicional. Você já está visualizando esse projeto, mas pode clicar aqui embaixo se quiser ver um pouco mais sobre o desenvolvimento ;).",
-        image: "/src/assets/imgs/portifolio.png",
+        image: "/assets/portifolio.png",
         images: [
 
-        "/src/assets/imgs/portifolio.png",
+        "/assets/portifolio.png",
 
         ],
         githubLink: "https://github.com/milacs1402/MeuPortifolio",
